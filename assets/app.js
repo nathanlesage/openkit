8d425d7124ae66b7bef38a7c4680ee418855cd62
Hendrik Erz <hendrik@zettlr.com>
2024-12-02 19:02:46 +0100

    feat: Save query in URL

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Starting app')
-
   const searchbar = document.querySelector('input#searchbar')
+
+  // Maybe prefill the searchbar
+  const search = new URLSearchParams(window.location.search)
+  const query = search.get('q')
+  if (query !== null) {
+    searchbar.value = query
+    filterTools(searchbar.value)
+  }
+
   searchbar.addEventListener('input', (event) => {
+    setQuery(searchbar.value)
     filterTools(searchbar.value)
   })
 })
 
-
+function setQuery (q) {
+  // Overwrite the history to save the current search field to the URL
+  const url = new URL(window.location.toString())
+  url.search = q.length > 0 ? '?q=' + encodeURIComponent(q) : ''
+  window.history.replaceState({ q }, q, url.toString())
+}
 
 function filterTools (query) {
   const descriptors = [...document.querySelectorAll('div.tool-descriptor')]
